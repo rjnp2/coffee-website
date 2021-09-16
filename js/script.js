bfea5e5d9e6f03986d3d08740eb0ec3c09b55d08
Rajan Pandey <58425689+rjnp2@users.noreply.github.com>
2021-09-16 14:51:04 +0545

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    cartItem.classList.remove('active');
+    searchbox.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchbox.classList.remove('active');
+}
+
+let searchbox = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchbox.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+    searchbox.classList.remove('active');
+}
